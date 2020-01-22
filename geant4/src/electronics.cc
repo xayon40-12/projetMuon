097fcc5df11a84cba8ba7684cb194f5636b5f443
CharlyLassalle <charlylassalle.cl@gmail.com>
2020-01-22 11:19:37 +0100

    fonctions gate, coincidence et anticoincidence

--- geant4/src/electronics.cc
+++ geant4/src/electronics.cc
@@ -15,6 +15,24 @@ std::vector<bool> gate(std::vector<double> &energy, double threshold, double gat
 return result;
 }
 
+
+
+
+std::vector<bool> coincidence(std::vector<bool> &energy1,std::vector<bool> &energy2, double gateTime, double binTime)
+  {
+    std::vector<bool> result(energy1.size());
+    for(int i=0;i<energy1.size();i++)
+      {
+	if(energy1[i] && energy2[i])
+	  {
+	    for (int max=i+(gateTime/binTime);i<energy.size() && i<max;i++){result[i]=true;}
+	  }
+	else{result[i]=false;}
+      }
+    return result;
+  }
+
+
 std::vector<bool> anti_coincidence(std::vector<bool> &energy1,std::vector<bool> &energy2, double gateTime, double binTime)
   {
     std::vector<bool> result(energy1.size());
